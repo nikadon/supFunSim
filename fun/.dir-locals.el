de295a7ed20698a56fd310d0fa406f0f638477cd
nikadon <nikadon@gmail.com>
2019-04-19 16:37:25 +0200

    added fun directory

---
+++ fun/.dir-locals.el
@@ -0,0 +1,12 @@
+(
+  (nil . (
+           (indent-tabs-mode . nil)
+           (fill-column . 80)
+           (tab-width . 4)
+           (c-basic-offset . 4)
+           ))
+
+  (c-mode . (
+              (c-file-style . "BSD")
+              ))
+  )
